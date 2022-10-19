565f9611974686e32495eaeb6eb17cd14201c916
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-19 22:43:48 +0000

    destructure and make es6 compliant , move connection from play to client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+const host = "::"
+const port = 50541
+
+const connect = () => {
+  const conn = net.createConnection({
+    host, // IP address here,
+    port // PORT number here,
+  });
+  conn.on("data", (serverData) => {
+    console.log(serverData);
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,19 +1,7 @@
-const net = require("net");
 
+const connect = require("./client")
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '::',// IP address here,
-    port: 50541 // PORT number here,
-  },);
- 
-conn.on("data", (serverData) => {
-  console.log(serverData)
-})
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  return conn;
-};
+
 
 console.log("Connecting ...");
 connect();
