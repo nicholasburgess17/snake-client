234299f554fcbe0c99f9cdcbfa965674e9081aff
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-19 22:12:20 +0000

    handle incoming data and log it to the console

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '::',// IP address here,
+    port: 50541 // PORT number here,
+  },);
+ 
+conn.on("data", (serverData) => {
+  console.log(serverData)
+})
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
