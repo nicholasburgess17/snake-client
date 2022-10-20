d5b2fa400cfd3334079067671bdc1b6cd1e5d5ac
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-20 00:51:45 +0000

    formatted

--- client.js
+++ client.js
@@ -1,25 +1,29 @@
 const net = require("net");
 
-const host = "::"
-const port = 50541
+const host = "::";
+const port = 50541;
 
 const connect = () => {
   const conn = net.createConnection({
     host, // IP address here,
-    port // PORT number here,
+    port, // PORT number here,
   });
   conn.on("connect", (connected) => {
-    console.log("Connected")
-    conn.write("Name: NB")
+    console.log("Connected");
+    conn.write("Name: NB");
+    conn.on("data", (serverData) => {
+      console.log(serverData);
+    });
+
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+    return conn;
   });
-  conn.on("data", (serverData) => {
-    console.log(serverData);
-  });
- 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  return conn;
-  
 };
 
 module.exports = connect;
+
+//"Move: up" - move up one square (unless facing down)
+//"Move: down" - move down one square (unless facing up)
+//"Move: left" - move left one square (unless facing right)
+//"Move: right" - move left one square (unless facing left)
--- play.js
+++ play.js
@@ -1,7 +1,6 @@
 
 const connect = require("./client")
 // establishes a connection with the game server
-
-
 console.log("Connecting ...");
 connect();
+
