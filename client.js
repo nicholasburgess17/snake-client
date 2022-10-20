6a53431c4395eae4a1126d83a549e7d307f18547
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-20 00:41:10 +0000

    name my snake

--- client.js
+++ client.js
@@ -8,12 +8,18 @@ const connect = () => {
     host, // IP address here,
     port // PORT number here,
   });
+  conn.on("connect", (connected) => {
+    console.log("Connected")
+    conn.write("Name: NB")
+  });
   conn.on("data", (serverData) => {
     console.log(serverData);
   });
+ 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   return conn;
+  
 };
 
 module.exports = connect;
